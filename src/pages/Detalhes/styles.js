5771021a68204f0b9ccf37c2873e55c7f1750de3
Rodrigo P Leite <rodrigopleite@yahoo.com.br>
2021-12-09 22:36:36 -0300

    Modificando Pagina Detalhes

--- src/pages/Detalhes/styles.js
+++ src/pages/Detalhes/styles.js
@@ -5,7 +5,6 @@ export const styles = StyleSheet.create({
       flex: 1,
       borderColor: '#45B39D',
       borderRadius: 10,
-      margin: 50,
       borderWidth: 5,
     },
 
@@ -21,17 +20,45 @@ export const styles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 1,
         borderColor:'black',
+        width: 40,
+        height: 28,
+        padding: 2,
+        marginTop: 20,
+        marginBottom: 20
+    },
+
+    btAdicionarCarrinho: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor:'black',
+        width: 230,
+        height: 40,
+        marginLeft: 52.5,
     },
 
     textoBt:{
         color: 'red',
         fontSize: 22,
         fontWeight: "bold",
-        resizeMode: 'contain'
     },
 
     textoQt: {
         fontSize: 22,
+        marginTop: 20,
+        marginRight: 40,
+        marginLeft: 50,
     },
+
+    espacoFotoProduto: {
+        margin: 10,
+    },
+
+    img: {
+        width: 320,
+        height: 200,
+        marginBottom: 5,
+      },
 
   });
