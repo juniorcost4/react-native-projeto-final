08ecb4cca9f842429c4ea9b01c1f49acf32a88a4
Erick-Ferraz <erickfpc.tech@gmail.com>
2021-12-10 16:02:30 -0300

    API funcionando no Heroku

--- service/Api/index.js
+++ service/Api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL:"http://10.0.0.115:8080/projetofinal"
+    baseURL:"https://serratec-apirest-projfinal.herokuapp.com/projetofinal/"
 });
 
 export default Api;
